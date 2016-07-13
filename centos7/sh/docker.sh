a43c13266dea95e8150e8fa1604bb98f288a5557
tudyzhb <tudyzhb@gmail.com>
2016-07-13 14:22:26 +0800

    docker config file

--- centos7/sh/docker.sh
+++ centos7/sh/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # docker
 sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
@@ -10,3 +11,34 @@ gpgkey=https://yum.dockerproject.org/gpg
 EOF
 sudo yum update
 sudo yum install -y docker-engine
+
+# docker config file
+sudo tee /etc/default/docker <<-'EOF'
+DOCKER_OPTS=" -g /data/docker/lib/docker/"
+EOF
+
+sudo tee /usr/lib/systemd/system/docker.service <<-'EOF'
+[Unit]
+Description=Docker Application Container Engine
+Documentation=https://docs.docker.com
+After=network.target docker.socket
+Requires=docker.socket
+
+[Service]
+Type=notify
+# the default is not to use systemd for cgroups because the delegate issues still
+# exists and systemd currently does not support the cgroup feature set required
+# for containers run by docker
+EnvironmentFile=-/etc/default/docker
+ExecStart=/usr/bin/docker daemon -H fd:// $DOCKER_OPTS
+MountFlags=slave
+LimitNOFILE=1048576
+LimitNPROC=1048576
+LimitCORE=infinity
+TimeoutStartSec=0
+# set delegate yes so that systemd does not reset the cgroups of docker containers
+Delegate=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
