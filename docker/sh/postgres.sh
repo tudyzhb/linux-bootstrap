ab1c0ce158ff7450712f272a5b56f1ab7e5f805e
tudyzhb <tudyzhb@gmail.com>
2016-07-25 16:18:22 +0800

    postgres

---
+++ docker/sh/postgres.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker stop pg
+docker rm pg
+docker run -d \
+--restart=always \
+--name pg \
+-p 5432:5432 \
+-e POSTGRES_PASSWORD=sergtsop \
+-v /data/docker/con/postgres/data:/var/lib/postgresql/data \
+postgres:latest
