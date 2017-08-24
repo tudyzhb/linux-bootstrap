0406f1bd866fb3fc7d0e3f3215b35793ddb56b98
tudyzhb <tudyzhb@gmail.com>
2017-08-24 13:00:39 +0800

    feat:supervisor reload

--- centos7/sh/supervisor.sh
+++ centos7/sh/supervisor.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 sudo easy_install supervisor
 sudo echo_supervisord_conf > /etc/supervisord.conf
+sudo mkdir /etc/supervisord.d
 sudo sed -i -- 's/minfds=1024/minfds=65535/g' /etc/supervisord.conf
 sudo echo "[include]" >> /etc/supervisord.conf
 sudo echo "files = supervisord.d/*.ini" >> /etc/supervisord.conf
 sudo tee /usr/lib/systemd/system/supervisord.service <<-'EOF'
 [Unit]
 Description=Process Monitoring and Control Daemon
-After=rc-local.service
+After=network.target
 
 [Service]
 Type=forking
-ExecStart=/usr/bin/supervisord -c /etc/supervisord.conf
+ExecStart=/bin/supervisord -c /etc/supervisord.conf
+ExecReload=/bin/supervisorctl reload
+ExecStop=/bin/supervisorctl shutdown
 SysVStartPriority=99
 
 [Install]
