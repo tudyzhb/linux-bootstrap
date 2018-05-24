db59bb9fc5d8309c38c09aec2c6ece696dc0dd84
tudyzhb <tudyzhb@gmail.com>
2018-05-25 01:03:12 +0800

    feat: docker build php

--- centos7/sh/docker_shell.sh
+++ centos7/sh/docker_shell.sh
@@ -2,6 +2,7 @@
 curl -fsSL https://get.docker.com/ | sh
 
 # docker config
+sudo mkdir /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
   "registry-mirrors": ["https://registry.docker-cn.com"],
