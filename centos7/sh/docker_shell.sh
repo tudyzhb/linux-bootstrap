fde177bfa7b7a68116807819ab6ea826495f8558
tudyzhb <tudyzhb@gmail.com>
2019-06-10 23:41:31 +0800

    feat: docker data dir selinux

--- centos7/sh/docker_shell.sh
+++ centos7/sh/docker_shell.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com/ | sh
 
+# selinux
+mkdir -p /data
+semanage fcontext -a -t var_t /data
+restorecon -v /data
+
 # docker config
 sudo mkdir /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
@@ -9,3 +14,7 @@ sudo tee /etc/docker/daemon.json <<-'EOF'
   "data-root": "/data/docker/lib/docker/"
 }
 EOF
+
+# start
+systemctl enable docker.service
+systemctl start docker.service
