855439e692dddf56d6a6abb2f550fc3df6f082db
tudyzhb <tudyzhb@gmail.com>
2020-03-16 11:15:18 +0800

    chore: docker native.cgroupdriver=systemd

--- centos7/sh/docker.sh
+++ centos7/sh/docker.sh
@@ -21,9 +21,19 @@ semanage fcontext -a -t var_t /data
 restorecon -v /data
 
 # docker config
+sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo mkdir /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ],
   "registry-mirrors": [
     "https://dockerhub.azk8s.cn",
     "https://hub-mirror.c.163.com"
