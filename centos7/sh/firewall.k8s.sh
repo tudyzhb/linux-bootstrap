04ba4fc334395873458828dc0f26adbd0aa1bf26
tudyzhb <tudyzhb@gmail.com>
2020-03-17 09:45:24 +0800

    fix: k8s flannel fiwewall

--- centos7/sh/firewall.k8s.sh
+++ centos7/sh/firewall.k8s.sh
@@ -6,3 +6,6 @@ firewall-cmd --permanent --add-port=10250/tcp --zone=public
 firewall-cmd --permanent --add-port=10251/tcp --zone=public
 firewall-cmd --permanent --add-port=10252/tcp --zone=public
 firewall-cmd --permanent --add-port=30000-32767/tcp --zone=public
+# flannel
+firewall-cmd --permanent --add-port=8472/udp --zone=public
+firewall-cmd --permanent --add-masquerade --zone=public
