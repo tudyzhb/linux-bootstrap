e7147b226464f7b41a196ebdc2c8c4877237b880
tudyzhb <tudyzhb@gmail.com>
2020-08-01 00:39:08 +0800

    chore: k8s nfs

--- centos7/sh/firewall.k8s.sh
+++ centos7/sh/firewall.k8s.sh
@@ -9,3 +9,5 @@ firewall-cmd --permanent --add-port=30000-32767/tcp --zone=public
 # flannel
 firewall-cmd --permanent --add-port=8472/udp --zone=public
 firewall-cmd --permanent --add-masquerade --zone=public
+# nfs
+yum install nfs-utils -y
