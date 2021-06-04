a68f64219e01f3c87ae61460cea71b5a579c852d
tudyzhb <tudyzhb@gmail.com>
2021-06-04 23:49:18 +0800

    feat: docker-compose binary download

--- centos7/sh/docker_compose.sh
+++ centos7/sh/docker_compose.sh
@@ -2,3 +2,6 @@
 sudo yum install epel-release
 sudo yum install -y python-pip
 sudo pip install docker-compose
+
+# binary
+curl https://github.com/docker/compose/releases/download/1.28.5/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
